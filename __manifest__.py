81c8c6e3042fafbcea41232d0cfc5591fea6b33e
Md. Shaheen Hossain <rapidgrps@princegroup-bd.com>
2025-04-08 21:51:03 +0600

    login with capchat 16 17 swiss

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Login/signup reCAPTCHA',
+    'version': '1.0',
+    'summary': """Login and signup with google reCAPTCHA""",
+    'description': """reCAPTCHA """,
+    'author': 'Md. Shaheen Hossain',
+    'company': 'Eagle ERP',
+    'website': 'https://www.eagle-erp.com',
+    'category': 'Extra Tools',
+    'depends': ['base'],
+    'license': 'LGPL-3',
+    'depends': ['base','web'],
+    'data': ['views/captcha_views.xml'],
+    'installable': True,
+    'auto_install': False,
+}
